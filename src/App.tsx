19afb4939d4ab84e1aeba971fc37150e7685379f
PrettyCoffee <jonathan-holz@outlook.com>
2022-11-06 17:40:40 +0100

    chore: Set up eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
+import reactLogo from "./assets/react.svg"
+
+import "./App.css"
+
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -30,5 +32,3 @@ function App() {
     </div>
   )
 }
-
-export default App
