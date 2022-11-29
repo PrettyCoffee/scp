901e9cb59dc62e301d0da897be8bf0e02e725552
PrettyCoffee <jonathan-holz@outlook.com>
2022-11-29 23:46:53 +0100

    feat: Add grid background

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,40 @@ import { spacing } from "./theme/spacing"
 
 export const gridSize = spacing.px.xl
 
+const GridBackground = styled.div`
+  position: relative;
+  ::before {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    inset: 0;
+    content: "";
+    mix-blend-mode: difference;
+    opacity: 0.5;
+
+    --grid-size: ${gridSize}px;
+    --stroke-color: white;
+    --stroke-width: 1px;
+
+    background-size: var(--grid-size) var(--grid-size);
+    background-image: repeating-linear-gradient(
+        0deg,
+        transparent,
+        transparent calc(var(--grid-size) - var(--stroke-width)),
+        var(--stroke-color) calc(var(--grid-size) - var(--stroke-width)),
+        var(--stroke-color) var(--grid-size)
+      ),
+      repeating-linear-gradient(
+        -90deg,
+        transparent,
+        transparent calc(var(--grid-size) - var(--stroke-width)),
+        var(--stroke-color) calc(var(--grid-size) - var(--stroke-width)),
+        var(--stroke-color) var(--grid-size)
+      );
+  }
+`
 const AppWrapper = styled.div`
-  ${({ theme: { space, tokens } }) => css`
-    padding: ${space.lg};
+  ${({ theme: { tokens } }) => css`
     text-align: center;
     color: ${tokens.text.default};
     background-color: ${tokens.background.base};
@@ -14,10 +45,18 @@ const AppWrapper = styled.div`
   `}
 `
 
+const Relative = styled(GridBackground)`
+  position: relative;
+  height: 100%;
+  width: 100%;
+`
+
 export const App = () => {
   return (
     <AppWrapper>
-      <Page />
+      <Relative>
+        <Page />
+      </Relative>
     </AppWrapper>
   )
 }
