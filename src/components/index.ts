a7134bcc75e058719e2605b2a0fb823031502127
PrettyCoffee <jonathan-holz@outlook.com>
2023-01-07 20:27:23 +0100

    feat(VisuallyHidden): Add component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./base"
 export * from "./layout"
+export * from "./primitives"
 export * from "./utility"
---
+++ src/components/primitives/VisuallyHidden.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren, useState } from "react"
+
+import { ClassNameProp } from "../base"
+import { useEventListener } from "../hooks"
+
+const Hidden = styled.span`
+  display: inline-block;
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+`
+
+export const VisuallyHidden = ({
+  children,
+  ...delegated
+}: PropsWithChildren<ClassNameProp>) => {
+  const [forceShow, setForceShow] = useState(false)
+
+  if (process.env.NODE_ENV !== "production") {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEventListener({
+      type: "keydown",
+      listener: ({ key }) => key === "Alt" && setForceShow(true),
+    })
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEventListener({
+      type: "keyup",
+      listener: ({ key }) => key === "Alt" && setForceShow(false),
+    })
+  }
+
+  return forceShow ? (
+    <>{children}</>
+  ) : (
+    <Hidden {...delegated}>{children}</Hidden>
+  )
+}
