e290ee90336b6ee09427827992e708065413a17e
PrettyCoffee <jonathan-holz@outlook.com>
2023-01-26 21:00:55 +0100

    refactor(Icon): Rename Cross to Close

--- src/components/primitives/icon/icons/Close.tsx
+++ src/components/primitives/icon/icons/Close.tsx
@@ -1,6 +1,6 @@
 import { IconDefinition, IconBase } from "./_IconBase"
 
-export const Cross: IconDefinition = props => (
+export const Close: IconDefinition = props => (
   <IconBase {...props}>
     <line x1="18" y1="6" x2="6" y2="18" />
     <line x1="6" y1="6" x2="18" y2="18" />
--- src/components/primitives/icon/icons/index.ts
+++ src/components/primitives/icon/icons/index.ts
@@ -1,6 +1,6 @@
 export type { IconDefinition } from "./_IconBase"
 
-export { Cross } from "./Cross"
+export { Close } from "./Close"
 export { Edit } from "./Edit"
 export { Grid } from "./Grid"
 export { Settings } from "./Settings"
