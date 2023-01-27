f09fffde6a4838eba0aae86b548138089f0ad030
PrettyCoffee <jonathan-holz@outlook.com>
2023-01-27 23:11:15 +0100

    feat: Add plus icon

---
+++ src/components/primitives/icon/icons/Plus.tsx
@@ -0,0 +1,8 @@
+import { IconDefinition, IconBase } from "./_IconBase"
+
+export const Plus: IconDefinition = props => (
+  <IconBase {...props}>
+    <line x1="12" y1="5" x2="12" y2="19" />
+    <line x1="5" y1="12" x2="19" y2="12" />
+  </IconBase>
+)
