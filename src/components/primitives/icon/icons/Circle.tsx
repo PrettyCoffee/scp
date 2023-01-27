fa4e5371b0742a258f28f6e75cebc80ea3ad076e
PrettyCoffee <jonathan-holz@outlook.com>
2023-01-27 23:07:31 +0100

    feat: Add circle icon

---
+++ src/components/primitives/icon/icons/Circle.tsx
@@ -0,0 +1,7 @@
+import { IconDefinition, IconBase } from "./_IconBase"
+
+export const Circle: IconDefinition = props => (
+  <IconBase {...props}>
+    <circle cx="12" cy="12" r="10" />
+  </IconBase>
+)
--- src/components/primitives/icon/icons/index.ts
+++ src/components/primitives/icon/icons/index.ts
@@ -1,5 +1,6 @@
 export type { IconDefinition } from "./_IconBase"
 
+export { Circle } from "./Circle"
 export { Close } from "./Close"
 export { Edit } from "./Edit"
 export { Grid } from "./Grid"
