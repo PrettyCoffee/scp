37df046ab18ee053818c6b07be966841d20b6d7b
PrettyCoffee <jonathan-holz@outlook.com>
2023-02-03 22:47:47 +0100

    feat(GeneralSettings): Introduce Accordion

--- src/page/GeneralSettingsMenu.tsx
+++ src/page/GeneralSettingsMenu.tsx
@@ -1,14 +1,57 @@
+import { useState } from "react"
+
+import { useTheme } from "@emotion/react"
+
 import {
+  Accordion,
+  AccordionState,
   Button,
   IconButton,
   Menu,
   Settings,
-  Text,
   TextArea,
   Trash,
 } from "../components"
 import { NumberInput } from "../components/inputs/NumberInput"
 import { useGeneralStore } from "../store"
+
+export const InputGrid = styled.div<{ columns?: number }>(
+  ({ theme: { space }, columns = 2 }) => css`
+    display: grid;
+    grid-template-columns: repeat(${columns}, 1fr);
+    gap: ${space.md};
+    > * {
+      flex: 1;
+    }
+  `
+)
+
+const SpacingEditor = () => {
+  const { gridSize, windowPadding, setStoreKey } = useGeneralStore()
+
+  const setGridSize = (value: number) => setStoreKey("gridSize", value)
+  const setWindowPadding = (value: number) =>
+    setStoreKey("windowPadding", value)
+
+  return (
+    <InputGrid columns={2}>
+      <NumberInput
+        label="Size of grid snapping"
+        placeholder="32"
+        unit="px"
+        value={gridSize}
+        onChange={setGridSize}
+      />
+      <NumberInput
+        label="Window padding"
+        placeholder="8"
+        unit="px"
+        value={windowPadding}
+        onChange={setWindowPadding}
+      />
+    </InputGrid>
+  )
+}
 
 const TileCssEditor = () => {
   const { globalTileCss, setStoreKey } = useGeneralStore()
@@ -26,40 +69,32 @@ const TileCssEditor = () => {
 
 const ResetButton = () => {
   const { resetStore } = useGeneralStore()
+  const { space } = useTheme()
   return (
-    <Button
-      look="danger"
-      icon={Trash}
-      caption="Reset settings"
-      onClick={resetStore}
-    />
+    <div style={{ paddingLeft: space.md, paddingTop: space.sm }}>
+      <Button
+        look="danger"
+        icon={Trash}
+        caption="Reset settings"
+        onClick={resetStore}
+      />
+    </div>
   )
 }
 
 const GeneralSettings = () => {
-  const { gridSize, windowPadding, setStoreKey } = useGeneralStore()
+  const [open, setOpen] = useState<AccordionState>("all")
 
-  const setGridSize = (value: number) => setStoreKey("gridSize", value)
-  const setWindowPadding = (value: number) =>
-    setStoreKey("windowPadding", value)
   return (
-    <>
-      <NumberInput
-        label="Size of grid snapping"
-        placeholder="32"
-        unit="px"
-        value={gridSize}
-        onChange={setGridSize}
-      />
-      <NumberInput
-        label="Window padding"
-        placeholder="8"
-        unit="px"
-        value={windowPadding}
-        onChange={setWindowPadding}
-      />
-      <TileCssEditor />
-    </>
+    <Accordion.Root open={open} onOpenChange={setOpen}>
+      <Accordion.Item label="Spacing">
+        <SpacingEditor />
+      </Accordion.Item>
+
+      <Accordion.Item label="Custom CSS">
+        <TileCssEditor />
+      </Accordion.Item>
+    </Accordion.Root>
   )
 }
 
@@ -77,12 +112,9 @@ export const GeneralSettingsMenu = () => (
       )}
     </Menu.Trigger>
     <Menu.Content>
-      <Menu.Header title="Settings" />
-      <Text.Medium>General settings that affect the global design.</Text.Medium>
+      <Menu.Header title="General Settings" />
       <GeneralSettings />
-      <div>
-        <ResetButton />
-      </div>
+      <ResetButton />
     </Menu.Content>
   </Menu.Root>
 )
