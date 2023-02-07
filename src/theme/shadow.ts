5248bb1ecd07d0eb65f489fb53f00e6a3dadd082
PrettyCoffee <jonathan-holz@outlook.com>
2023-02-07 20:47:20 +0100

    feat(theme): Add shadow color as css variable

--- src/theme/shadow.ts
+++ src/theme/shadow.ts
@@ -1,22 +1,23 @@
 export const shadow = {
+  color: "240deg 36% 5%",
   small: `
-    0px 0px 0.5px rgba(8, 8, 17, 0.45),
-    0px 0px 4px -2px rgba(8, 8, 17, 0.45),
-    0px 0px 12px -6px rgba(8, 8, 17, 0.45)`,
+    0px 0px 0.5px hsla(var(--shadow-color) / 0.45),
+    0px 0px 4px -2px hsla(var(--shadow-color) / 0.45),
+    0px 0px 12px -6px hsla(var(--shadow-color) / 0.45)`,
 
   medium: `
-    0px 0px 0.8px rgba(8, 8, 17, 0.4),
-    0px 0px 2px -0.6px rgba(8, 8, 17, 0.4),
-    0px 0px 4px -1.2px rgba(8, 8, 17, 0.4),
-    0px 0px 8px -1.8px rgba(8, 8, 17, 0.4),
-    0px 0px 16px -2.4px rgba(8, 8, 17, 0.4)`,
+    0px 0px 0.8px hsla(var(--shadow-color) / 0.4),
+    0px 0px 2px -0.6px hsla(var(--shadow-color) / 0.4),
+    0px 0px 4px -1.2px hsla(var(--shadow-color) / 0.4),
+    0px 0px 8px -1.8px hsla(var(--shadow-color) / 0.4),
+    0px 0px 16px -2.4px hsla(var(--shadow-color) / 0.4)`,
 
   large: `
-    0px 0px 0.8px rgba(8, 8, 17, 0.35),
-    0px 0px 5px -0.4px rgba(8, 8, 17, 0.35),
-    0px 0px 10px -0.8px rgba(8, 8, 17, 0.35),
-    0px 0px 18px -1.2px rgba(8, 8, 17, 0.35),
-    0px 0px 30px -1.6px rgba(8, 8, 17, 0.35),
-    0px 0px 50px -2px rgba(8, 8, 17, 0.35),
-    0px 0px 80px -2.5px rgba(8, 8, 17, 0.35)`,
+    0px 0px 0.8px hsla(var(--shadow-color) / 0.35),
+    0px 0px 5px -0.4px hsla(var(--shadow-color) / 0.35),
+    0px 0px 10px -0.8px hsla(var(--shadow-color) / 0.35),
+    0px 0px 18px -1.2px hsla(var(--shadow-color) / 0.35),
+    0px 0px 30px -1.6px hsla(var(--shadow-color) / 0.35),
+    0px 0px 50px -2px hsla(var(--shadow-color) / 0.35),
+    0px 0px 80px -2.5px hsla(var(--shadow-color) / 0.35)`,
 }
